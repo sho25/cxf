d7d37d5c1a797d28a60d402930e58a2675a2be50
Willem Ning Jiang <ningjiang@apache.org>
2009-01-17 08:39:42 +0000

    Fixed the CS error in ws-security
    git-svn-id: https://svn.apache.org/repos/asf/cxf/trunk@735251 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 319f4067fd13dbfaeeb4ade1966d324d9107de0b

--- rt/ws/security/src/main/java/org/apache/cxf/ws/security/wss4j/policyhandlers/AbstractBindingBuilder.java
+++ rt/ws/security/src/main/java/org/apache/cxf/ws/security/wss4j/policyhandlers/AbstractBindingBuilder.java
@@ -47,16 +47,6 @@ name|java
 operator|.
 name|security
 operator|.
-name|Key
-import|;
-end_import
-
-begin_import
-import|import
-name|java
-operator|.
-name|security
-operator|.
 name|KeyStoreException
 import|;
 end_import
