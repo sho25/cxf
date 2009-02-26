10ccf6fbb247d54457e322211e9f25b2d9976e26
Eoghan Glynn <eglynn@apache.org>
2009-02-26 16:30:45 +0000

    Properly skipping MAPTest.testFallbackThreadPoolConfig
    git-svn-id: https://svn.apache.org/repos/asf/cxf/trunk@748199 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: ab0d80ccb04d32a75d7e66c394156a223cb962cf

--- systests/src/test/java/org/apache/cxf/systest/ws/addressing/MAPTest.java
+++ systests/src/test/java/org/apache/cxf/systest/ws/addressing/MAPTest.java
@@ -49,6 +49,16 @@ name|org
 operator|.
 name|junit
 operator|.
+name|Ignore
+import|;
+end_import
+
+begin_import
+import|import
+name|org
+operator|.
+name|junit
+operator|.
 name|Test
 import|;
 end_import
@@ -257,6 +267,13 @@ throw|;
 block|}
 block|}
 block|}
+annotation|@
+name|Test
+annotation|@
+name|Ignore
+argument_list|(
+literal|"move to separate test"
+argument_list|)
 specifier|public
 name|void
 name|testFallbackThreadPoolConfig
