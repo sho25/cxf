bb3d188185360be566f5e0abe0d589d5a8f4aaad
Benson Margulies <bimargulies@apache.org>
2009-03-21 15:54:10 +0000

    CXF-2125. I don't know why I'm the only person to run into this.
    git-svn-id: https://svn.apache.org/repos/asf/cxf/trunk@756960 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 99b9ceeb8a69112e53003450e5c3fd14bcc31e84

--- rt/bindings/soap/src/main/java/org/apache/cxf/binding/soap/saaj/SAAJInInterceptor.java
+++ rt/bindings/soap/src/main/java/org/apache/cxf/binding/soap/saaj/SAAJInInterceptor.java
@@ -649,6 +649,28 @@ operator|.
 name|class
 argument_list|)
 decl_stmt|;
+if|if
+condition|(
+name|node
+operator|==
+literal|null
+condition|)
+block|{
+comment|// replicate 2.1 behavior.
+name|part
+operator|.
+name|setContent
+argument_list|(
+operator|new
+name|DOMSource
+argument_list|(
+literal|null
+argument_list|)
+argument_list|)
+expr_stmt|;
+block|}
+else|else
+block|{
 name|StaxUtils
 operator|.
 name|copy
@@ -662,6 +684,7 @@ name|part
 argument_list|)
 argument_list|)
 expr_stmt|;
+block|}
 comment|// TODO: setup mime headers
 name|Collection
 argument_list|<
