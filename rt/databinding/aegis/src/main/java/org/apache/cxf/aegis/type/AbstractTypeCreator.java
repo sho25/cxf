d396bbd3b6d3de76b5c25330431167f7b0a9a507
J. Daniel Kulp <dkulp@apache.org>
2009-03-31 14:01:59 +0000

    Remove dependency on jaxws from aegis
    git-svn-id: https://svn.apache.org/repos/asf/cxf/trunk@760456 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: bbd48b767eea44ff50b8f237b61ce5ee1714f909

--- rt/databinding/aegis/src/main/java/org/apache/cxf/aegis/type/AbstractTypeCreator.java
+++ rt/databinding/aegis/src/main/java/org/apache/cxf/aegis/type/AbstractTypeCreator.java
@@ -85,18 +85,6 @@ end_import
 
 begin_import
 import|import
-name|javax
-operator|.
-name|xml
-operator|.
-name|ws
-operator|.
-name|Holder
-import|;
-end_import
-
-begin_import
-import|import
 name|org
 operator|.
 name|apache
@@ -685,13 +673,14 @@ name|javaType
 parameter_list|)
 block|{
 return|return
-name|javaType
+literal|"javax.xml.ws.Holder"
 operator|.
 name|equals
 argument_list|(
-name|Holder
+name|javaType
 operator|.
-name|class
+name|getName
+argument_list|()
 argument_list|)
 return|;
 block|}
