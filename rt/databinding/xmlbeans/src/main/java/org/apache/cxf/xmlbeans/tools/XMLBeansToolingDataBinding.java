f2a070d4b65297fd8cf3769743e5649b6606366e
J. Daniel Kulp <dkulp@apache.org>
2009-06-22 15:59:16 +0000

    CHeckstyle errors
    git-svn-id: https://svn.apache.org/repos/asf/cxf/trunk@787290 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: a66d93f00bc7e363e6d77b46e306c6935bcb2ded

--- rt/databinding/xmlbeans/src/main/java/org/apache/cxf/xmlbeans/tools/XMLBeansToolingDataBinding.java
+++ rt/databinding/xmlbeans/src/main/java/org/apache/cxf/xmlbeans/tools/XMLBeansToolingDataBinding.java
@@ -43,16 +43,6 @@ name|java
 operator|.
 name|io
 operator|.
-name|InputStream
-import|;
-end_import
-
-begin_import
-import|import
-name|java
-operator|.
-name|io
-operator|.
 name|Writer
 import|;
 end_import
@@ -216,20 +206,6 @@ operator|.
 name|helpers
 operator|.
 name|XMLUtils
-import|;
-end_import
-
-begin_import
-import|import
-name|org
-operator|.
-name|apache
-operator|.
-name|cxf
-operator|.
-name|staxutils
-operator|.
-name|StaxUtils
 import|;
 end_import
 
