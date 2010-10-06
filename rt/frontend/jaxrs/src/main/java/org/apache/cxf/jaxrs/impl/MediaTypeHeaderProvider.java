ade9ded73496a378ce80bc6de3926db05d3e9c0d
Sergey Beryozkin <sergeyb@apache.org>
2010-10-06 22:20:36 +0000

    Minor update to MediaType handler
    git-svn-id: https://svn.apache.org/repos/asf/cxf/trunk@1005275 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 9d1e013e537cd61435e1d174a06767f82455720a

--- rt/frontend/jaxrs/src/main/java/org/apache/cxf/jaxrs/impl/MediaTypeHeaderProvider.java
+++ rt/frontend/jaxrs/src/main/java/org/apache/cxf/jaxrs/impl/MediaTypeHeaderProvider.java
@@ -141,7 +141,7 @@ name|Pattern
 operator|.
 name|compile
 argument_list|(
-literal|"(([\\w-]+=\"[^\"]*\")|([\\w-]+=[\\w-]+))"
+literal|"(([\\w-]+=\"[^\"]*\")|([\\w-]+=[\\w-/]+))"
 argument_list|)
 decl_stmt|;
 specifier|public
