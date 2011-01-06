a7884b1784e405450c2d99010c2e8c4bb94180f5
Willem Ning Jiang <ningjiang@apache.org>
2011-01-06 10:25:39 +0000

    Fixed a CS error of wsdl_first_soap12 example
    git-svn-id: https://svn.apache.org/repos/asf/cxf/trunk@1055817 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: d4822274b1bc00f3f2f1b85db170c1ec96c39529

--- distribution/src/main/release/samples/wsdl_first_soap12/src/demo/hw/server/Server.java
+++ distribution/src/main/release/samples/wsdl_first_soap12/src/demo/hw/server/Server.java
@@ -15,18 +15,6 @@ end_package
 
 begin_import
 import|import
-name|javax
-operator|.
-name|xml
-operator|.
-name|ws
-operator|.
-name|Endpoint
-import|;
-end_import
-
-begin_import
-import|import
 name|org
 operator|.
 name|apache
