8a611384da41dcff13645ee93065a1e0529ceada
Christian Schneider <cschneider@apache.org>
2011-02-19 10:53:49 +0000

    Moving http tests for authentication to auth package
    git-svn-id: https://svn.apache.org/repos/asf/cxf/trunk@1072306 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: b4c35d8350c7057cb978522ac44d3cf2ad61b97e

--- rt/transports/http/src/test/java/org/apache/cxf/transport/http/auth/HttpAuthHeaderTest.java
+++ rt/transports/http/src/test/java/org/apache/cxf/transport/http/auth/HttpAuthHeaderTest.java
@@ -14,6 +14,8 @@ operator|.
 name|transport
 operator|.
 name|http
+operator|.
+name|auth
 package|;
 end_package
 
@@ -34,24 +36,6 @@ operator|.
 name|util
 operator|.
 name|Map
-import|;
-end_import
-
-begin_import
-import|import
-name|org
-operator|.
-name|apache
-operator|.
-name|cxf
-operator|.
-name|transport
-operator|.
-name|http
-operator|.
-name|auth
-operator|.
-name|HttpAuthHeader
 import|;
 end_import
 
