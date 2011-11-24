74d865a44d2ea19ff1981462ba1ed340609da06c
Benson Margulies <bimargulies@apache.org>
2011-11-24 22:19:29 +0000

    Add parameter for mapping missing parts to null.
    git-svn-id: https://svn.apache.org/repos/asf/cxf/trunk@1205996 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: b8f6b3ab93aebf1e6d2d92a25736b35bf6d2e93b

--- rt/frontend/jaxrs/src/main/java/org/apache/cxf/jaxrs/ext/multipart/Multipart.java
+++ rt/frontend/jaxrs/src/main/java/org/apache/cxf/jaxrs/ext/multipart/Multipart.java
@@ -67,6 +67,10 @@ name|Target
 import|;
 end_import
 
+begin_comment
+comment|/**  * Annotate a JAX-RS function parameter to receive data from a multipart 'part'.  **/
+end_comment
+
 begin_annotation_defn
 annotation|@
 name|Target
@@ -96,17 +100,26 @@ specifier|public
 annotation_defn|@interface
 name|Multipart
 block|{
+comment|/**      * The name of the MIME part to map to this parameter. The default is      * the unnamed default part.      **/
 name|String
 name|value
 parameter_list|()
 default|default
 literal|""
 function_decl|;
+comment|/**      * Select the part by MIME type. The default is to match any MIME type.      */
 name|String
 name|type
 parameter_list|()
 default|default
 literal|"*/*"
+function_decl|;
+comment|/**      * How to handle a missing part. By default, if no part matches,      * the {@link org.apache.cxf.jaxrs.provider.MultipartProvider}       * throws a {@link javax.ws.rs.WebApplicationException}      * with status 404. If this option is set to<strong>false</strong>,      * the parameter is set to<strong>null</strong>.      */
+name|boolean
+name|errorIfMissing
+parameter_list|()
+default|default
+literal|true
 function_decl|;
 block|}
 end_annotation_defn
