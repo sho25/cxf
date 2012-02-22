5c32b550df2a19d2715e08a2afa41b8a0a46857c
Sergey Beryozkin <sergeyb@apache.org>
2012-02-22 17:44:04 +0000

    [CXF-4112] Initial OAuth 2.0 Authorization Code support
    git-svn-id: https://svn.apache.org/repos/asf/cxf/trunk@1292416 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: daef913ce4b9d150b8f921753d6dc585bd127acb

---
+++ rt/rs/security/oauth-parent/oauth2/src/main/java/org/apache/cxf/rs/security/oauth/utils/OAuthConstants.java
@@ -0,0 +1,42 @@
+begin_unit|revision:0.9.5;language:Java;cregit-version:0.0.1
+begin_comment
+comment|/**  * Licensed to the Apache Software Foundation (ASF) under one  * or more contributor license agreements. See the NOTICE file  * distributed with this work for additional information  * regarding copyright ownership. The ASF licenses this file  * to you under the Apache License, Version 2.0 (the  * "License"); you may not use this file except in compliance  * with the License. You may obtain a copy of the License at  *  * http://www.apache.org/licenses/LICENSE-2.0  *  * Unless required by applicable law or agreed to in writing,  * software distributed under the License is distributed on an  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY  * KIND, either express or implied. See the License for the  * specific language governing permissions and limitations  * under the License.  */
+end_comment
+
+begin_package
+package|package
+name|org
+operator|.
+name|apache
+operator|.
+name|cxf
+operator|.
+name|rs
+operator|.
+name|security
+operator|.
+name|oauth
+operator|.
+name|utils
+package|;
+end_package
+
+begin_comment
+comment|/**  * Miscellaneous constants   */
+end_comment
+
+begin_class
+specifier|public
+specifier|final
+class|class
+name|OAuthConstants
+block|{
+specifier|private
+name|OAuthConstants
+parameter_list|()
+block|{     }
+block|}
+end_class
+
+end_unit
+
