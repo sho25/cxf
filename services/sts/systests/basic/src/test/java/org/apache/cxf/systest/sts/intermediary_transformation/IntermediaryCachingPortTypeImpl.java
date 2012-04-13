9d759d04cb44a20c898801073b9c1d54120be8fe
Colm O Heigeartaigh <coheigea@apache.org>
2012-04-13 12:12:00 +0000

    Fixing failing STS interop test with the metro STS
    git-svn-id: https://svn.apache.org/repos/asf/cxf/trunk@1325729 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 490df0e9464be154cbe1236c6eadf12268d36309

--- services/sts/systests/basic/src/test/java/org/apache/cxf/systest/sts/intermediary_transformation/IntermediaryCachingPortTypeImpl.java
+++ services/sts/systests/basic/src/test/java/org/apache/cxf/systest/sts/intermediary_transformation/IntermediaryCachingPortTypeImpl.java
@@ -532,16 +532,19 @@ argument_list|)
 expr_stmt|;
 name|Assert
 operator|.
-name|assertEquals
+name|assertTrue
 argument_list|(
-literal|"Princiapl must be alice"
-argument_list|,
-literal|"alice"
+literal|"Principal must be alice"
 argument_list|,
 name|pr
 operator|.
 name|getName
 argument_list|()
+operator|.
+name|contains
+argument_list|(
+literal|"alice"
+argument_list|)
 argument_list|)
 expr_stmt|;
 comment|// Disable the STSClient after the first invocation
