7989a4467a810d5ee1856348c407dd7f1823eb2a
Daniel Kulp <dkulp@apache.org>
2012-05-19 00:47:52 +0000

    Left a sleep in
    git-svn-id: https://svn.apache.org/repos/asf/cxf/trunk@1340324 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 1c0c700b481a19f9f605dc1c349b5766b59f869f

--- systests/uncategorized/src/test/java/org/apache/cxf/systest/wsdl/CrossSchemaImportsTests.java
+++ systests/uncategorized/src/test/java/org/apache/cxf/systest/wsdl/CrossSchemaImportsTests.java
@@ -186,13 +186,6 @@ parameter_list|()
 throws|throws
 name|Exception
 block|{
-name|Thread
-operator|.
-name|sleep
-argument_list|(
-literal|100000
-argument_list|)
-expr_stmt|;
 name|testUtilities
 operator|.
 name|setBus
