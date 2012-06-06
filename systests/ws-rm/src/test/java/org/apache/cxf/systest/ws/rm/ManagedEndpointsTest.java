f4cf5508216711233c943b6c1a7c47ef204412c0
Daniel Kulp <dkulp@apache.org>
2012-06-06 14:58:22 +0000

    Fix checkstyle issue
    git-svn-id: https://svn.apache.org/repos/asf/cxf/trunk@1346925 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 2bfdf50c76057f0ccf25d780c14bfb08e1116b03

--- systests/ws-rm/src/test/java/org/apache/cxf/systest/ws/rm/ManagedEndpointsTest.java
+++ systests/ws-rm/src/test/java/org/apache/cxf/systest/ws/rm/ManagedEndpointsTest.java
@@ -25,16 +25,6 @@ name|java
 operator|.
 name|util
 operator|.
-name|Arrays
-import|;
-end_import
-
-begin_import
-import|import
-name|java
-operator|.
-name|util
-operator|.
 name|Date
 import|;
 end_import
