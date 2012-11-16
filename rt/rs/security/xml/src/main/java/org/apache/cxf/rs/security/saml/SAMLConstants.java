6b6db6f4cd714f61864375c5a95e002d7939901c
Colm O Heigeartaigh <coheigea@apache.org>
2012-11-16 16:56:56 +0000

    [CXF-4638][CXF-4639] - Add ability to set STSClient Claims via a CallbackHandler  - Add ability to send an existing SAML Token via the JAX-RS SAML code
    git-svn-id: https://svn.apache.org/repos/asf/cxf/trunk@1410467 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 7ceda035062a8e559e1eab69de6f19e224ba1824

---
+++ rt/rs/security/xml/src/main/java/org/apache/cxf/rs/security/saml/SAMLConstants.java
@@ -0,0 +1,51 @@
+begin_unit|revision:0.9.5;language:Java;cregit-version:0.0.1
+begin_comment
+comment|/**  * Licensed to the Apache Software Foundation (ASF) under one  * or more contributor license agreements. See the NOTICE file  * distributed with this work for additional information  * regarding copyright ownership. The ASF licenses this file  * to you under the Apache License, Version 2.0 (the  * "License"); you may not use this file except in compliance  * with the License. You may obtain a copy of the License at  *  * http://www.apache.org/licenses/LICENSE-2.0  *  * Unless required by applicable law or agreed to in writing,  * software distributed under the License is distributed on an  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY  * KIND, either express or implied. See the License for the  * specific language governing permissions and limitations  * under the License.  */
+end_comment
+
+begin_package
+package|package
+name|org
+operator|.
+name|apache
+operator|.
+name|cxf
+operator|.
+name|rs
+operator|.
+name|security
+operator|.
+name|saml
+package|;
+end_package
+
+begin_comment
+comment|/**  * Some constant configuration options  */
+end_comment
+
+begin_class
+specifier|public
+specifier|final
+class|class
+name|SAMLConstants
+block|{
+comment|/**      * This tag refers to a DOM Element representation of a SAML Token. If a SAML Token      * is stored on the Message Context, then the SamlFormOutInterceptor and       * SamlHeaderOutInterceptor will use this token instead of creating a new SAML Token.      */
+specifier|public
+specifier|static
+specifier|final
+name|String
+name|SAML_TOKEN_ELEMENT
+init|=
+literal|"rs-security.saml.token.element"
+decl_stmt|;
+specifier|private
+name|SAMLConstants
+parameter_list|()
+block|{
+comment|// complete
+block|}
+block|}
+end_class
+
+end_unit
+
