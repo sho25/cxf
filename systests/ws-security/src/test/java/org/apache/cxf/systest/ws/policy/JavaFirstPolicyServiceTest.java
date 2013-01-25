bf9924a348e0793285ca35783749c97b6c194ce6
Colm O Heigeartaigh <coheigea@apache.org>
2013-01-25 10:26:30 +0000

    [CXF-4776] - Fix + re-enable tests
    git-svn-id: https://svn.apache.org/repos/asf/cxf/trunk@1438424 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 295a4e2f9eb3e7e0513980202949ccc424dee2d4

--- systests/ws-security/src/test/java/org/apache/cxf/systest/ws/policy/JavaFirstPolicyServiceTest.java
+++ systests/ws-security/src/test/java/org/apache/cxf/systest/ws/policy/JavaFirstPolicyServiceTest.java
@@ -448,12 +448,6 @@ operator|.
 name|junit
 operator|.
 name|Test
-annotation|@
-name|org
-operator|.
-name|junit
-operator|.
-name|Ignore
 specifier|public
 name|void
 name|testUsernameTokenInterceptorNoPasswordValidation
@@ -625,12 +619,6 @@ operator|.
 name|junit
 operator|.
 name|Test
-annotation|@
-name|org
-operator|.
-name|junit
-operator|.
-name|Ignore
 specifier|public
 name|void
 name|testUsernameTokenPolicyValidatorNoPasswordValidation
