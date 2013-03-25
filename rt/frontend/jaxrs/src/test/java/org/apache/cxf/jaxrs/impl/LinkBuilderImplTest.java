f9a63b1b119468971f8065fa4c08551ad94bd972
Sergey Beryozkin <sergeyb@apache.org>
2013-03-25 16:09:20 +0000

    Fixing the checkstyle errors, disabling one more test for now
    git-svn-id: https://svn.apache.org/repos/asf/cxf/trunk@1460742 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 8cf99a503f801d153e1ae09ebb42694d23de9026

--- rt/frontend/jaxrs/src/test/java/org/apache/cxf/jaxrs/impl/LinkBuilderImplTest.java
+++ rt/frontend/jaxrs/src/test/java/org/apache/cxf/jaxrs/impl/LinkBuilderImplTest.java
@@ -1,4 +1,8 @@
 begin_unit|revision:0.9.5;language:Java;cregit-version:0.0.1
+begin_comment
+comment|/**  * Licensed to the Apache Software Foundation (ASF) under one  * or more contributor license agreements. See the NOTICE file  * distributed with this work for additional information  * regarding copyright ownership. The ASF licenses this file  * to you under the Apache License, Version 2.0 (the  * "License"); you may not use this file except in compliance  * with the License. You may obtain a copy of the License at  *  * http://www.apache.org/licenses/LICENSE-2.0  *  * Unless required by applicable law or agreed to in writing,  * software distributed under the License is distributed on an  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY  * KIND, either express or implied. See the License for the  * specific language governing permissions and limitations  * under the License.  */
+end_comment
+
 begin_package
 package|package
 name|org
@@ -12,18 +16,6 @@ operator|.
 name|impl
 package|;
 end_package
-
-begin_import
-import|import static
-name|org
-operator|.
-name|junit
-operator|.
-name|Assert
-operator|.
-name|assertEquals
-import|;
-end_import
 
 begin_import
 import|import
@@ -69,10 +61,31 @@ name|Test
 import|;
 end_import
 
+begin_import
+import|import static
+name|org
+operator|.
+name|junit
+operator|.
+name|Assert
+operator|.
+name|assertEquals
+import|;
+end_import
+
 begin_class
 specifier|public
 class|class
-name|TestLinkBuilderImpl
+name|LinkBuilderImplTest
+block|{
+annotation|@
+name|Test
+specifier|public
+name|void
+name|build
+parameter_list|()
+throws|throws
+name|Exception
 block|{
 name|Link
 operator|.
@@ -83,15 +96,6 @@ operator|new
 name|LinkBuilderImpl
 argument_list|()
 decl_stmt|;
-annotation|@
-name|Test
-specifier|public
-name|void
-name|build
-parameter_list|()
-throws|throws
-name|Exception
-block|{
 name|Link
 name|prevLink
 init|=
@@ -122,6 +126,11 @@ argument_list|)
 expr_stmt|;
 block|}
 annotation|@
+name|Ignore
+argument_list|(
+literal|"Ignored due to CXF-4919"
+argument_list|)
+annotation|@
 name|Test
 specifier|public
 name|void
@@ -130,6 +139,15 @@ parameter_list|()
 throws|throws
 name|Exception
 block|{
+name|Link
+operator|.
+name|Builder
+name|linkBuilder
+init|=
+operator|new
+name|LinkBuilderImpl
+argument_list|()
+decl_stmt|;
 name|URI
 name|base
 init|=
@@ -181,6 +199,15 @@ throws|throws
 name|Exception
 block|{
 name|Link
+operator|.
+name|Builder
+name|linkBuilder
+init|=
+operator|new
+name|LinkBuilderImpl
+argument_list|()
+decl_stmt|;
+name|Link
 name|prevLink
 init|=
 name|linkBuilder
@@ -223,6 +250,15 @@ parameter_list|()
 throws|throws
 name|Exception
 block|{
+name|Link
+operator|.
+name|Builder
+name|linkBuilder
+init|=
+operator|new
+name|LinkBuilderImpl
+argument_list|()
+decl_stmt|;
 name|Link
 name|prevLink
 init|=
@@ -295,6 +331,15 @@ parameter_list|()
 throws|throws
 name|Exception
 block|{
+name|Link
+operator|.
+name|Builder
+name|linkBuilder
+init|=
+operator|new
+name|LinkBuilderImpl
+argument_list|()
+decl_stmt|;
 name|URI
 name|base
 init|=
