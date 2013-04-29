18ae33ee0c28bad4725c80fe04737dfd84ee9a30
Colm O Heigeartaigh <coheigea@apache.org>
2013-04-29 14:27:16 +0000

    [CXF-4991] EH-Cache availability checks in ReplayCacheFactory and TokenStoreFactory now performed using the defining classloader
    Conflicts:
    
    	rt/ws/security/src/main/java/org/apache/cxf/ws/security/cache/ReplayCacheFactory.java
    
    git-svn-id: https://svn.apache.org/repos/asf/cxf/branches/wss4j2.0-port@1477099 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: ebd2889e35dddb29935436e0bc3b8b2560b9e23f

--- rt/ws/security/src/main/java/org/apache/cxf/ws/security/tokenstore/TokenStoreFactory.java
+++ rt/ws/security/src/main/java/org/apache/cxf/ws/security/tokenstore/TokenStoreFactory.java
@@ -124,15 +124,11 @@ name|?
 argument_list|>
 name|cacheManagerClass
 init|=
-name|ClassLoaderUtils
+name|Class
 operator|.
-name|loadClass
+name|forName
 argument_list|(
 literal|"net.sf.ehcache.CacheManager"
-argument_list|,
-name|TokenStoreFactory
-operator|.
-name|class
 argument_list|)
 decl_stmt|;
 if|if
