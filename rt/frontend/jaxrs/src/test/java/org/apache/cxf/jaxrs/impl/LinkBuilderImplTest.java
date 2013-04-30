1ff881a9d55d9bdca593f31a44f6c89b3c98cf14
Sergey Beryozkin <sergeyb@apache.org>
2013-04-30 15:23:40 +0000

    [CXF-4919] Fixing UriInfo/HttpUtils relativize implementation, quality patch from Stian Soiland-Reyes applied
    git-svn-id: https://svn.apache.org/repos/asf/cxf/trunk@1477686 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: f0939c91e9adaee10baac0b8960135b96e35843c

--- rt/frontend/jaxrs/src/test/java/org/apache/cxf/jaxrs/impl/LinkBuilderImplTest.java
+++ rt/frontend/jaxrs/src/test/java/org/apache/cxf/jaxrs/impl/LinkBuilderImplTest.java
@@ -47,16 +47,6 @@ name|org
 operator|.
 name|junit
 operator|.
-name|Ignore
-import|;
-end_import
-
-begin_import
-import|import
-name|org
-operator|.
-name|junit
-operator|.
 name|Test
 import|;
 end_import
@@ -125,11 +115,6 @@ argument_list|()
 argument_list|)
 expr_stmt|;
 block|}
-annotation|@
-name|Ignore
-argument_list|(
-literal|"Ignored due to CXF-4919"
-argument_list|)
 annotation|@
 name|Test
 specifier|public
@@ -317,11 +302,6 @@ argument_list|()
 argument_list|)
 expr_stmt|;
 block|}
-annotation|@
-name|Ignore
-argument_list|(
-literal|"Ignored due to CXF-4919"
-argument_list|)
 annotation|@
 name|Test
 specifier|public
