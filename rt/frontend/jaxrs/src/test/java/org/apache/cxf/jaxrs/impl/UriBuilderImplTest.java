8e895ae8f008fa7b8d01b2f5a9aa0604c2b20e9c
Sergey Beryozkin <sergeyb@apache.org>
2013-07-30 12:47:46 +0000

    Re-enabling a couple of UriBuilderTests
    git-svn-id: https://svn.apache.org/repos/asf/cxf/trunk@1508424 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 3b29f849442ee58765c57c3e22ff7b4c3b476d6a

--- rt/frontend/jaxrs/src/test/java/org/apache/cxf/jaxrs/impl/UriBuilderImplTest.java
+++ rt/frontend/jaxrs/src/test/java/org/apache/cxf/jaxrs/impl/UriBuilderImplTest.java
@@ -177,16 +177,6 @@ name|org
 operator|.
 name|junit
 operator|.
-name|Ignore
-import|;
-end_import
-
-begin_import
-import|import
-name|org
-operator|.
-name|junit
-operator|.
 name|Test
 import|;
 end_import
@@ -9609,8 +9599,6 @@ argument_list|)
 expr_stmt|;
 block|}
 annotation|@
-name|Ignore
-annotation|@
 name|Test
 specifier|public
 name|void
@@ -9903,8 +9891,6 @@ argument_list|()
 argument_list|)
 expr_stmt|;
 block|}
-annotation|@
-name|Ignore
 annotation|@
 name|Test
 specifier|public
