f61b0eb610443f462fb216df27cc37ec65bb0534
Christian Schneider <cschneider@apache.org>
2013-08-06 17:36:42 +0000

    CXF-5121 Introduce  XACMLAuthorizingInterceptor
    git-svn-id: https://svn.apache.org/repos/asf/cxf/trunk@1511048 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: d0c54eacbda2b96eae6175acfd5265288b8722bf

--- rt/security/src/main/java/org/apache/cxf/rt/security/xacml/AbstractXACMLAuthorizingInterceptor.java
+++ rt/security/src/main/java/org/apache/cxf/rt/security/xacml/AbstractXACMLAuthorizingInterceptor.java
@@ -342,10 +342,12 @@ import|;
 end_import
 
 begin_comment
-comment|/**  * An abstract interceptor to perform an XACML authorization request to a remote PDP,  * and make an authorization decision based on the response. It takes the principal and roles  * from the SecurityContext, and uses the XACMLRequestBuilder to construct an XACML Request  * statement.   *   * This class must be subclassed to actually perform the request to the PDP.  */
+comment|/**  * An abstract interceptor to perform an XACML authorization request to a remote PDP,  * and make an authorization decision based on the response. It takes the principal and roles  * from the SecurityContext, and uses the XACMLRequestBuilder to construct an XACML Request  * statement.   *   * This class must be subclassed to actually perform the request to the PDP.  *   * @deprecated: Use XACMLAuthorizingInterceptor instead  */
 end_comment
 
 begin_class
+annotation|@
+name|Deprecated
 specifier|public
 specifier|abstract
 class|class
