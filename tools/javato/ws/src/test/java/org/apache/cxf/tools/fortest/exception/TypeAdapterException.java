ccef43ca816209a31ed96356e4ee05e71161e051
Daniel Kulp <dkulp@apache.org>
2013-08-23 21:32:19 +0000

    Fix eclipse warning
    git-svn-id: https://svn.apache.org/repos/asf/cxf/trunk@1517058 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 767962594fbdf7ed04aca5857f966bf271c3a053

--- tools/javato/ws/src/test/java/org/apache/cxf/tools/fortest/exception/TypeAdapterException.java
+++ tools/javato/ws/src/test/java/org/apache/cxf/tools/fortest/exception/TypeAdapterException.java
@@ -43,6 +43,14 @@ extends|extends
 name|Exception
 block|{
 specifier|private
+specifier|static
+specifier|final
+name|long
+name|serialVersionUID
+init|=
+literal|1L
+decl_stmt|;
+specifier|private
 name|MyClass1
 name|adapted
 decl_stmt|;
