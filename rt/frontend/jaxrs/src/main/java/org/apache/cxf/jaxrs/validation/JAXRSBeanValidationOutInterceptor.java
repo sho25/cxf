4297e4999cb8018169caf09fdb270a7eaeee35cf
Sergey Beryozkin <sergeyb@apache.org>
2013-12-05 11:58:40 +0000

    [CXF-5309] Minor updates to JAX-RS bean validators
    git-svn-id: https://svn.apache.org/repos/asf/cxf/trunk@1548099 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: a754fecdb8e5aec4b9a6fcfca34000b98bc60b01

--- rt/frontend/jaxrs/src/main/java/org/apache/cxf/jaxrs/validation/JAXRSBeanValidationOutInterceptor.java
+++ rt/frontend/jaxrs/src/main/java/org/apache/cxf/jaxrs/validation/JAXRSBeanValidationOutInterceptor.java
@@ -85,6 +85,20 @@ end_import
 
 begin_import
 import|import
+name|javax
+operator|.
+name|ws
+operator|.
+name|rs
+operator|.
+name|ext
+operator|.
+name|Provider
+import|;
+end_import
+
+begin_import
+import|import
 name|org
 operator|.
 name|apache
@@ -126,6 +140,8 @@ import|;
 end_import
 
 begin_class
+annotation|@
+name|Provider
 specifier|public
 class|class
 name|JAXRSBeanValidationOutInterceptor
