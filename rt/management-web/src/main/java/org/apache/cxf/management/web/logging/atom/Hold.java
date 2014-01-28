b8481a9308f3c599a530172215f02f28d01be108
Daniel Kulp <dkulp@apache.org>
2014-01-28 21:24:35 +0000

    Add a retained annotation to make sure package-info.class is created to avoid re-compiles
    git-svn-id: https://svn.apache.org/repos/asf/cxf/trunk@1562237 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 7408a9319936861d7d8bf35a0a9e8e3f534a4834

--- rt/management-web/src/main/java/org/apache/cxf/management/web/logging/atom/Hold.java
+++ rt/management-web/src/main/java/org/apache/cxf/management/web/logging/atom/Hold.java
@@ -3,15 +3,6 @@ begin_comment
 comment|/**  * Licensed to the Apache Software Foundation (ASF) under one  * or more contributor license agreements. See the NOTICE file  * distributed with this work for additional information  * regarding copyright ownership. The ASF licenses this file  * to you under the Apache License, Version 2.0 (the  * "License"); you may not use this file except in compliance  * with the License. You may obtain a copy of the License at  *  * http://www.apache.org/licenses/LICENSE-2.0  *  * Unless required by applicable law or agreed to in writing,  * software distributed under the License is distributed on an  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY  * KIND, either express or implied. See the License for the  * specific language governing permissions and limitations  * under the License.  */
 end_comment
 
-begin_comment
-comment|/**  * Support for producing logs in  *<a href="http://tools.ietf.org/html/rfc4287">ATOM Syndication Format</a>.  * Allows to configure<tt>java.util.logging</tt> (JUL) loggers to use  * handlers producing ATOM feeds that are either pushed to or pulled by client.    */
-end_comment
-
-begin_annotation
-annotation|@
-name|Hold
-end_annotation
-
 begin_package
 package|package
 name|org
@@ -29,6 +20,80 @@ operator|.
 name|atom
 package|;
 end_package
+
+begin_import
+import|import
+name|java
+operator|.
+name|lang
+operator|.
+name|annotation
+operator|.
+name|ElementType
+import|;
+end_import
+
+begin_import
+import|import
+name|java
+operator|.
+name|lang
+operator|.
+name|annotation
+operator|.
+name|Retention
+import|;
+end_import
+
+begin_import
+import|import
+name|java
+operator|.
+name|lang
+operator|.
+name|annotation
+operator|.
+name|RetentionPolicy
+import|;
+end_import
+
+begin_import
+import|import
+name|java
+operator|.
+name|lang
+operator|.
+name|annotation
+operator|.
+name|Target
+import|;
+end_import
+
+begin_comment
+comment|/*  * Marker annotation to force the package-info.java to be compiled by javac   * to avoid problems with the maven compiler plugin always compiling everything  */
+end_comment
+
+begin_annotation_defn
+annotation|@
+name|Retention
+argument_list|(
+name|RetentionPolicy
+operator|.
+name|RUNTIME
+argument_list|)
+annotation|@
+name|Target
+argument_list|(
+block|{
+name|ElementType
+operator|.
+name|PACKAGE
+block|}
+argument_list|)
+annotation_defn|@interface
+name|Hold
+block|{  }
+end_annotation_defn
 
 end_unit
 
