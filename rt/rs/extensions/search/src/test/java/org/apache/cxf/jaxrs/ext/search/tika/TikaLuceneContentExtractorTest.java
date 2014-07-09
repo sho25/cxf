17506ea3c4ebd0fccd1a1d6e7dafb10fda933a75
reta <drreta@gmail.com>
2014-07-08 21:23:58 -0400

    CXF-5549: Introduce Tika Search Visitor. Refactored contextual property date retrieval
    Former-commit-id: f55a02f157b94b7781ff72811372d99cb15244a6

--- rt/rs/extensions/search/src/test/java/org/apache/cxf/jaxrs/ext/search/tika/TikaLuceneContentExtractorTest.java
+++ rt/rs/extensions/search/src/test/java/org/apache/cxf/jaxrs/ext/search/tika/TikaLuceneContentExtractorTest.java
@@ -359,16 +359,6 @@ name|org
 operator|.
 name|junit
 operator|.
-name|Ignore
-import|;
-end_import
-
-begin_import
-import|import
-name|org
-operator|.
-name|junit
-operator|.
 name|Test
 import|;
 end_import
@@ -575,8 +565,6 @@ expr_stmt|;
 block|}
 annotation|@
 name|Test
-annotation|@
-name|Ignore
 specifier|public
 name|void
 name|testExtractedTextContentMatchesTypesAndDateSearchCriteria
