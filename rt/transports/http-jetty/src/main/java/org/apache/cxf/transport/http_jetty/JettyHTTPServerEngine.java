0fa4fc4af49b8a1dd0d5f2af17d67f258376e3dc
Daniel Kulp <dkulp@apache.org>
2014-08-15 13:33:13 -0400

    Remove a suppresswarnings that won't be necessary until we go to Jetty9
    Former-commit-id: d54a21628143bc15f51cbe5d63fa42c09d0ab8ac

--- rt/transports/http-jetty/src/main/java/org/apache/cxf/transport/http_jetty/JettyHTTPServerEngine.java
+++ rt/transports/http-jetty/src/main/java/org/apache/cxf/transport/http_jetty/JettyHTTPServerEngine.java
@@ -1571,11 +1571,6 @@ operator|new
 name|ErrorHandler
 argument_list|()
 block|{
-annotation|@
-name|SuppressWarnings
-argument_list|(
-literal|"deprecation"
-argument_list|)
 specifier|public
 name|void
 name|handle
