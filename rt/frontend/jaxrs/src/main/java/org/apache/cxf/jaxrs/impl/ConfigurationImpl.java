962a7ba7cf2977b5b81d9e3e848d6288120da8e2
Sergey Beryozkin <sberyozkin@talend.com>
2014-09-05 10:25:27 +0100

    Using a linked hash map to store JAX-RS providers in ConfigurationImpl
    Former-commit-id: 9189ee95f59a006ccdbd6968c85e182b3cabd973

--- rt/frontend/jaxrs/src/main/java/org/apache/cxf/jaxrs/impl/ConfigurationImpl.java
+++ rt/frontend/jaxrs/src/main/java/org/apache/cxf/jaxrs/impl/ConfigurationImpl.java
@@ -63,6 +63,16 @@ name|java
 operator|.
 name|util
 operator|.
+name|LinkedHashMap
+import|;
+end_import
+
+begin_import
+import|import
+name|java
+operator|.
+name|util
+operator|.
 name|Map
 import|;
 end_import
@@ -180,7 +190,7 @@ argument_list|>
 name|providers
 init|=
 operator|new
-name|HashMap
+name|LinkedHashMap
 argument_list|<
 name|Object
 argument_list|,
@@ -206,7 +216,7 @@ argument_list|>
 name|features
 init|=
 operator|new
-name|HashMap
+name|LinkedHashMap
 argument_list|<
 name|Feature
 argument_list|,
