3d1f37727c8b4335e830dbaeda13eac305a5b8f0
Sergey Beryozkin <sberyozkin@talend.com>
2015-02-23 16:45:31 +0000

    [CXF-6132] Moving the initializer to a package different from the jaxrs frontend package and also moving META-INF resource from systests to the http-sci module
    Former-commit-id: 8077aaa7e2968f53978b7deb3f9582c0279d3901

--- rt/rs/http-sci/src/main/java/org/apache/cxf/jaxrs/servlet/sci/JaxrsServletContainerInitializer.java
+++ rt/rs/http-sci/src/main/java/org/apache/cxf/jaxrs/servlet/sci/JaxrsServletContainerInitializer.java
@@ -14,6 +14,8 @@ operator|.
 name|jaxrs
 operator|.
 name|servlet
+operator|.
+name|sci
 package|;
 end_package
 
@@ -230,6 +232,22 @@ operator|.
 name|logging
 operator|.
 name|LogUtils
+import|;
+end_import
+
+begin_import
+import|import
+name|org
+operator|.
+name|apache
+operator|.
+name|cxf
+operator|.
+name|jaxrs
+operator|.
+name|servlet
+operator|.
+name|CXFNonSpringJaxrsServlet
 import|;
 end_import
 
