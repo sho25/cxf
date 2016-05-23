a57d0a2dd8e6e4b25f1b8c917ee504bff7f6a85e
Sergey Beryozkin <sberyozkin@gmail.com>
2016-05-23 13:34:41 +0100

    [CXF-6725] Removing deprecated JWS JSON properties, likely never used as they were not documented
    Former-commit-id: 5e11c6d98783a2a46e54f67eebe5a1b414414073

--- rt/rs/security/jose-parent/jose/src/main/java/org/apache/cxf/rs/security/jose/common/JoseConstants.java
+++ rt/rs/security/jose-parent/jose/src/main/java/org/apache/cxf/rs/security/jose/common/JoseConstants.java
@@ -375,39 +375,6 @@ name|RSSEC_SIGNATURE_PROPS
 init|=
 literal|"rs.security.signature.properties"
 decl_stmt|;
-comment|/**      * The OLD signature properties file for JSON Serialization signature creation.       * If not specified then it falls back to RSSEC_SIGNATURE_LIST_PROPS.      * Use RSSEC_SIGNATURE_OUT_PROPS instead      */
-annotation|@
-name|Deprecated
-specifier|public
-specifier|static
-specifier|final
-name|String
-name|DEP_RSSEC_SIGNATURE_OUT_LIST_PROPS
-init|=
-literal|"rs.security.signature.out.list.properties"
-decl_stmt|;
-comment|/**      * The OLD signature properties file for JSON Serialization signature verification.       * If not specified then it falls back to RSSEC_SIGNATURE_LIST_PROPS.      * Use RSSEC_SIGNATURE_IN_PROPS instead      */
-annotation|@
-name|Deprecated
-specifier|public
-specifier|static
-specifier|final
-name|String
-name|DEP_RSSEC_SIGNATURE_IN_LIST_PROPS
-init|=
-literal|"rs.security.signature.in.list.properties"
-decl_stmt|;
-comment|/**      * The OLD signature properties file for JSON Serialization signature creation/verification.      * Use RSSEC_SIGNATURE_PROPS instead      */
-annotation|@
-name|Deprecated
-specifier|public
-specifier|static
-specifier|final
-name|String
-name|DEP_RSSEC_SIGNATURE_LIST_PROPS
-init|=
-literal|"rs.security.signature.list.properties"
-decl_stmt|;
 comment|/**      * Include the JWK public key for signature in the "jwk" header.       */
 specifier|public
 specifier|static
