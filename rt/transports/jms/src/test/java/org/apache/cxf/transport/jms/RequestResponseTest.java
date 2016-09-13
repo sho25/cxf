d50e102d6f654277c05220a1d039be53634c831a
Freeman Fang <freeman.fang@gmail.com>
2016-09-13 15:54:00 +0800

    [CXF-7051]RequestResponseTest.testRequestTopicResponseStaticQueue intermittently failed with java9
    Former-commit-id: 573405dd523139bb935339b89c28f849676da727

--- rt/transports/jms/src/test/java/org/apache/cxf/transport/jms/RequestResponseTest.java
+++ rt/transports/jms/src/test/java/org/apache/cxf/transport/jms/RequestResponseTest.java
@@ -143,16 +143,6 @@ name|org
 operator|.
 name|junit
 operator|.
-name|Ignore
-import|;
-end_import
-
-begin_import
-import|import
-name|org
-operator|.
-name|junit
-operator|.
 name|Test
 import|;
 end_import
@@ -406,7 +396,7 @@ argument_list|)
 expr_stmt|;
 block|}
 annotation|@
-name|Ignore
+name|Test
 specifier|public
 name|void
 name|testRequestTopicResponseStaticQueue
