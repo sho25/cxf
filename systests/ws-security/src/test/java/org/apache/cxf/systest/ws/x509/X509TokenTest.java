d4880cb85b5e63ea3ddd6c9b0ecd57009644616c
Colm O hEigeartaigh <coheigea@apache.org>
2017-02-02 11:05:27 +0000

    Fixing WS-Addressing + WS-Security test
    Former-commit-id: b4fa7851f6c0050c3433814f1ba095c4bea89102

--- systests/ws-security/src/test/java/org/apache/cxf/systest/ws/x509/X509TokenTest.java
+++ systests/ws-security/src/test/java/org/apache/cxf/systest/ws/x509/X509TokenTest.java
@@ -9487,12 +9487,6 @@ operator|.
 name|junit
 operator|.
 name|Test
-annotation|@
-name|org
-operator|.
-name|junit
-operator|.
-name|Ignore
 specifier|public
 name|void
 name|testSymmetricWithOptionalAddressing
