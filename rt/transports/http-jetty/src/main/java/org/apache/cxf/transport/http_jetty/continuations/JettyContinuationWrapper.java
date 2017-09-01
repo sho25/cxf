a5676d9d61e0f89adbd38f4dc023b16607359665
Daniel Kulp <dkulp@apache.org>
2017-09-01 15:23:58 -0400

    Update to Jetty 9.4.x
    Former-commit-id: 171dc6c8c177887905465c16b1f6c3f572c08b56

--- rt/transports/http-jetty/src/main/java/org/apache/cxf/transport/http_jetty/continuations/JettyContinuationWrapper.java
+++ rt/transports/http-jetty/src/main/java/org/apache/cxf/transport/http_jetty/continuations/JettyContinuationWrapper.java
@@ -130,6 +130,11 @@ import|;
 end_import
 
 begin_class
+annotation|@
+name|SuppressWarnings
+argument_list|(
+literal|"deprecation"
+argument_list|)
 specifier|public
 class|class
 name|JettyContinuationWrapper
