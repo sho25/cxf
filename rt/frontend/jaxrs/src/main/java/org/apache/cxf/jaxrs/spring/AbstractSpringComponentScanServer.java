bbb03f4ae22ce64bb8d43bb56aca13ddbde0bdfa
Dennis Kieselhorst <mail@dekies.de>
2018-11-06 14:05:40 +0100

    use isEmpty()
    Former-commit-id: 138752922f10e6cddd3ac16972a5d9d0649a1513

--- rt/frontend/jaxrs/src/main/java/org/apache/cxf/jaxrs/spring/AbstractSpringComponentScanServer.java
+++ rt/frontend/jaxrs/src/main/java/org/apache/cxf/jaxrs/spring/AbstractSpringComponentScanServer.java
@@ -809,12 +809,11 @@ argument_list|)
 decl_stmt|;
 if|if
 condition|(
+operator|!
 name|apps
 operator|.
-name|size
+name|isEmpty
 argument_list|()
-operator|>
-literal|0
 condition|)
 block|{
 name|factoryBean
