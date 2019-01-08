15ad449121f57a61448b2efc4ad83453bfaa49c0
Colm O hEigeartaigh <coheigea@apache.org>
2019-01-08 09:45:10 +0000

    Fixing build failure
    Former-commit-id: e3aa32f540d47685165f191ca02fc203452bd5b2

--- systests/uncategorized/src/test/java/org/apache/cxf/systest/transform/feature/TransformFeatureTest.java
+++ systests/uncategorized/src/test/java/org/apache/cxf/systest/transform/feature/TransformFeatureTest.java
@@ -175,6 +175,18 @@ name|assertTrue
 import|;
 end_import
 
+begin_import
+import|import static
+name|org
+operator|.
+name|junit
+operator|.
+name|Assert
+operator|.
+name|fail
+import|;
+end_import
+
 begin_class
 specifier|public
 class|class
