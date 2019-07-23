903bdbac275b8357d0a50c0af8e534f334f9ff4e
Amos Feng <zfeng@redhat.com>
2019-07-23 15:23:45 +0800

    CXF-8079 Update to introduce the wsat transaction example
    Former-commit-id: a50d2d81dd7d620061ac21b6df7943ad5c6c92cc

---
+++ distribution/src/main/release/samples/ws_transaction/client/src/test/java/sample/ws/service/SampleWsApplication.java
@@ -0,0 +1,44 @@
+begin_unit|revision:0.9.5;language:Java;cregit-version:0.0.1
+begin_comment
+comment|/*  * JBoss, Home of Professional Open Source.  * Copyright 2019, Red Hat, Inc., and individual contributors  * as indicated by the @author tags. See the copyright.txt file in the  * distribution for a full listing of individual contributors.  *  * This is free software; you can redistribute it and/or modify it  * under the terms of the GNU Lesser General Public License as  * published by the Free Software Foundation; either version 2.1 of  * the License, or (at your option) any later version.  *  * This software is distributed in the hope that it will be useful,  * but WITHOUT ANY WARRANTY; without even the implied warranty of  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU  * Lesser General Public License for more details.  *  * You should have received a copy of the GNU Lesser General Public  * License along with this software; if not, write to the Free  * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  * 02110-1301 USA, or see the FSF site: http://www.fsf.org.  */
+end_comment
+
+begin_package
+package|package
+name|sample
+operator|.
+name|ws
+operator|.
+name|service
+package|;
+end_package
+
+begin_import
+import|import
+name|org
+operator|.
+name|springframework
+operator|.
+name|boot
+operator|.
+name|autoconfigure
+operator|.
+name|SpringBootApplication
+import|;
+end_import
+
+begin_comment
+comment|/**  * @author<a href="mailto:zfeng@redhat.com">Zheng Feng</a>  */
+end_comment
+
+begin_class
+annotation|@
+name|SpringBootApplication
+specifier|public
+class|class
+name|SampleWsApplication
+block|{ }
+end_class
+
+end_unit
+
